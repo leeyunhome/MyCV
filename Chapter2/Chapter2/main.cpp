62a0713ac4f8731af8e4ce5a7e3ba1156b21f971
leeyunhome <leeyunhome@daum.net>
2020-02-06 18:26:59 +0900

    waitKey() test

--- Chapter2/Chapter2/main.cpp
+++ Chapter2/Chapter2/main.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 int main()
 {
-	Mat src = imread("bd.png");
+	Mat src = imread("bd.png", IMREAD_REDUCED_COLOR_2);
 
 	vector<int> params;
 	params.push_back(IMWRITE_JPEG_QUALITY);
-	params.push_back(1);
+	params.push_back(5);
 	imwrite("lenna.jpg", src, params);
-	Mat dst = imread("lenna.jpg");
+	Mat dst = imread("lenna.jpg", IMREAD_REDUCED_COLOR_2);
 
 	if (src.empty())
 	{
@@ -23,10 +23,22 @@ int main()
 		return -1;
 	}
 
-	imshow("src", src);
-	imshow("dst", dst);
+	namedWindow("원조", WINDOW_NORMAL);
+	namedWindow("복사본", WINDOW_NORMAL);
+	moveWindow("원조", 100, 200);
+	resizeWindow("복사본", 50, 550);
+	imshow("원조", src);
+	imshow("복사본", dst);
 
-	waitKey();
+	int flag;
+	//flag = waitKey(20000);
+	//cout << flag << endl;
+
+	if ((flag = waitKey(0)) == 'q')
+	{
+		cout << flag << endl;
+		destroyAllWindows();
+	}
 
 	return 0;
 }
